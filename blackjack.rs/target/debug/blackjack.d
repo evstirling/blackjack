00cd8345b91297a8caa74da84e9d4623063da100
evstirling <evanstirling@gmail.com>
2022-08-02 01:11:28 -0230

    Updated naming conventions

--- blackjack.rs/target/debug/blackjack.d
+++
@@ -1,1 +0,0 @@
-/home/ev/programming/blackjack/blackjack.rs/target/debug/blackjack: /home/ev/programming/blackjack/blackjack.rs/src/main.rs
--- blackjack.rs/target/debug/deps/blackjack-57b15dc4d7193d3a.d
+++
@@ -1,5 +0,0 @@
-/home/ev/programming/blackjack/blackjack.rs/target/debug/deps/blackjack-57b15dc4d7193d3a: src/main.rs
-
-/home/ev/programming/blackjack/blackjack.rs/target/debug/deps/blackjack-57b15dc4d7193d3a.d: src/main.rs
-
-src/main.rs:
